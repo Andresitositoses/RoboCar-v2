b5356cbbfa3dd686658ab9e990cab60d88115f9e
Andrés <andreschicanofernandez@hotmail.com>
2022-04-23 12:46:00 +0200

    The entire project has been modified to make use of Azure RTOS ThreadX
    main.cpp has been converted to main.c and now it is so much easier to configure new peripherals. It is because main function now resides in app_threadx.cpp file, which is the function thread named mainThread_entry. Besides, ive been create 3 new print funtions for C++.

---
+++ Core/Src/app_threadx.cpp
@@ -0,0 +1,145 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file    app_threadx.c
+ * @author  MCD Application Team
+ * @brief   ThreadX applicative file
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "app_threadx.h"
+#include "main.h"
+#include "string"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+#define MAINTHREAD_STACK_SIZE 1024
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+// Handlers
+extern UART_HandleTypeDef huart1;
+extern TIM_HandleTypeDef htim4;
+extern TIM_HandleTypeDef htim16;
+
+// Main thread
+uint8_t mainThread_stack[MAINTHREAD_STACK_SIZE];
+TX_THREAD mainThread_ptr;
+
+// Rest of threads...
+// ...
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+// Print functions
+void print(char *str);
+void print(float value);
+void print(char* str, float value);
+
+// Threads functions
+VOID mainThread_entry(ULONG initial_input);
+
+/* USER CODE END PFP */
+
+/**
+ * @brief  Application ThreadX Initialization.
+ * @param memory_ptr: memory pointer
+ * @retval int
+ */
+UINT App_ThreadX_Init(VOID *memory_ptr) {
+	UINT ret = TX_SUCCESS;
+	TX_BYTE_POOL *byte_pool = (TX_BYTE_POOL*) memory_ptr;
+
+	/* USER CODE BEGIN App_ThreadX_MEM_POOL */
+	(void) byte_pool;
+	/* USER CODE END App_ThreadX_MEM_POOL */
+
+	/* USER CODE BEGIN App_ThreadX_Init */
+	tx_thread_create(&mainThread_ptr, "mainThread", mainThread_entry, 0,
+			mainThread_stack, MAINTHREAD_STACK_SIZE, 15, 15, 1, TX_AUTO_START);
+	/* USER CODE END App_ThreadX_Init */
+
+	return ret;
+}
+
+/**
+ * @brief  MX_ThreadX_Init
+ * @param  None
+ * @retval None
+ */
+void MX_ThreadX_Init(void) {
+	/* USER CODE BEGIN  Before_Kernel_Start */
+
+	/* USER CODE END  Before_Kernel_Start */
+
+	tx_kernel_enter();
+
+	/* USER CODE BEGIN  Kernel_Start_Error */
+
+	/* USER CODE END  Kernel_Start_Error */
+}
+
+/* USER CODE BEGIN 1 */
+VOID mainThread_entry(ULONG initial_input) {
+	float numero = 12.3f;
+	while (1) {
+		print("Valor: ", numero);
+		HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+		tx_thread_sleep(10); // 10 cs -> 0.1 s -> 100 ms
+	}
+}
+
+
+// Print functions for C++
+
+void print(char *str) {
+	std::string cadena = str;
+	for (unsigned int i = 0; i < cadena.length(); i++) {
+		HAL_UART_Transmit(&huart1, (uint8_t*) &cadena.at(i), 1, HAL_MAX_DELAY);
+	}
+}
+
+void print(float num) {
+	std::string cadena = std::to_string(num);
+	for (unsigned int i = 0; i < cadena.length(); i++) {
+		HAL_UART_Transmit(&huart1, (uint8_t*) &cadena.at(i), 1, HAL_MAX_DELAY);
+	}
+}
+
+void print(char *str, float num) {
+	print(str);
+	print(num);
+	print("\n");
+}
